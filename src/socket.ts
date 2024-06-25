9f1af106f3b73d57ce88a7f9036cead02cb2650b
Keshav Kumar <keshav.k987@gmail.com>
2024-06-25 20:01:12 +0530

    sudo code for doing auth and saving socket connection

--- src/socket.ts
+++ src/socket.ts
@@ -1,9 +1,40 @@
+import { IncomingMessage } from "http";
 import WebSocket from "ws";
+import url from "url";
+
+interface ClientInfo {
+  socket: WebSocket;
+  username: string;
+}
+
+const clients: Map<string, ClientInfo> = new Map();
 
 const initializeWebSocketServer = (port: number) => {
-  const wss = new WebSocket.Server({ port });
+  const wss: WebSocket.Server = new WebSocket.Server({ port });
 
-  wss.on("connection", (ws: WebSocket) => {
+  wss.on("connection", (ws: WebSocket, req: IncomingMessage) => {
+    const query = url.parse(req.url || "", true).query;
+    const token = query.token as string;
+
+    console.log(`token is ${token}`);
+
+    const username = "Keshav";
+    const clientInfo: ClientInfo = { socket: ws, username };
+
+    const clientInfoString = JSON.stringify(clientInfo);
+    console.log(clientInfoString);
+
+    clients.set(username, clientInfo);
+
+    // task: validate token
+    // if (!token || !validateToken(token)) {
+    //     ws.close(4001, "Unauthorized"); // Close connection with an error code and reason
+    //     return;
+    //   }
+
+    // Save the client information,
+    //  clients.set(userId, { socket: ws, userId, username });
+
     console.log("New client connected");
 
     ws.on("message", (message: string) => {
